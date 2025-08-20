4f026fe3aa7da00ca0c9bd04a59f984d270480a6
phasuk15 <phasukdianas15@gmail.com>
2025-08-20 23:06:44 +0100

    Essay description section

--- app/sections/Essays.tsx
+++ app/sections/Essays.tsx
@@ -1,7 +1,8 @@
 const Essays = () => {
     return (  
-        <div>
-            
+        <div className="h-screen max-w-2xl mx-auto px-4 flex flex-col items-center">
+            <h1 className="font-jersey font-bold text-5xl py-12">Essays</h1>
+            <p className="font-pixelify text-center leading-relaxed">Little windows into my thoughts — essays on things I find fascinating or worth pondering 💭🌸</p>
         </div>
     );
 }
