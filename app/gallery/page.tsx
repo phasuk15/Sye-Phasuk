bf255c440f1d3fff8abd0f3fba9f5005f37e2b67
phasuk15 <phasukdianas15@gmail.com>
2025-08-21 01:01:06 +0100

    Description of full gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,7 +1,8 @@
 const GalleryFull = () => {
     return (  
-        <div>
-            Gallery
+        <div className="h-screen max-w-2xl mx-auto px-4 flex flex-col items-center">
+            <h1 className="font-jersey font-bold text-5xl pt-24 pb-6">Gallery</h1>
+            <p className="font-pixelify text-center">Welcome to my art gallery! Creating artwork has been a passionate hobby of mine for as long as I can remember. I've always enjoyed exploring different styles and various mediums and now including pixel art! (I drew all the assets on this webpage hehe) 🌷✨  </p>
         </div>
     );
 }
--- app/sections/Gallery.tsx
+++ app/sections/Gallery.tsx
@@ -3,6 +3,12 @@ const Gallery = () => {
         <div className="h-screen max-w-2xl mx-auto px-4 flex flex-col items-center">
             <h1 className="font-jersey font-bold text-5xl py-12">Gallery</h1>
             <p className="font-pixelify text-center leading-relaxed">Take a peek into a cozy little gallery of my creations! 🖌️🎨</p>
+            <a
+                href="/gallery"
+                className="flex justify-center text-center font-jersey text-2xl mt-10"
+            >
+                See full gallery
+            </a>
         </div>
     );
 }
