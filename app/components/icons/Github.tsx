7b8ca0d4c9a5f6f58116bd99c38b8902cf2b5b2a
phasuk15 <phasukdianas15@gmail.com>
2025-09-18 00:56:17 +0100

    Fixed title page

--- app/components/icons/Github.tsx
+++ app/components/icons/Github.tsx
@@ -12,7 +12,7 @@ const Github = () => {
                 alt="github"
                 width={32}
                 height={32}
-            className="hover:opacity-30 transition"
+                className="hover:opacity-30 transition"
             />
         </a>
     );
--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -7,14 +7,14 @@ import Linkedin from "../components/icons/Linkedin";
 const Hero = () => {
   return (  
     <div className="w-screen h-screen flex flex-col md:flex-row">
-      <div className="relative h-full flex flex-col justify-end md:justify-center items-center md:items-start px-12 w-full md:w-1/3 p-6">
-        <p className="text-gray-500 font-pixelify text-xl md:text-left md:text-2xl">
+      <div className="relative h-full flex flex-col justify-end md:justify-center items-center md:items-end px-12 w-full md:w-1/2 p-6">
+        <p className="text-gray-500 font-pixelify text-xl md:text-2xl">
           Hello! I am
         </p>
         <h1 
           data-aos="fade-up" 
           data-aos-once="false" 
-          className="text-gray-300 text-8xl font-jersey font-bold text-center md:text-left md:text-9xl"
+          className="text-raspberry text-8xl font-jersey font-bold text-center md:text-right md:text-9xl"
         >
           Sye Phasuk
         </h1>
@@ -29,7 +29,7 @@ const Hero = () => {
       </div>
 
       {/* Right side (image) */}
-      <div className="relative flex justify-center items-center h-1/2 md:h-full w-full md:w-2/3 mt-8">
+      <div className="relative flex justify-center items-center h-1/2 md:h-full w-full md:w-1/2 mt-8">
         <Image 
           src="/avatar.png" 
           alt="hero" 
