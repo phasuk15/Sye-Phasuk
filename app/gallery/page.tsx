8a2463a234ee7a5e2f110959daf74a381f05bf2f
phasuk15 <phasukdianas15@gmail.com>
2025-10-15 14:29:36 +0100

    Added artworks

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -6,6 +6,8 @@ const GalleryFull = () => {
   const artworks = [
     { src: "/artwork10.jpg", caption: "Artwork 10" },
     { src: "/artwork11.jpg", caption: "Artwork 11" },
+    { src: "/artwork12.jpg", caption: "Artwork 12" },
+    { src: "/artwork13.jpg", caption: "Artwork 13" },
     { src: "/artwork1.jpg", caption: "Artwork 1" },
     { src: "/artwork2.jpg", caption: "Artwork 2" },
     { src: "/artwork3.jpg", caption: "Artwork 3" },
